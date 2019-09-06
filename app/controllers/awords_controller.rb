899ca5ee30f2c51f594cec8fdd9a7f2806843927
Unknown <kuzelenkov@gmail.com>
2019-09-06 09:55:11 +0300

    Основные страницы

--- app/controllers/awords_controller.rb
+++ app/controllers/awords_controller.rb
@@ -1,2 +1,7 @@
 class AwordsController < ApplicationController
+  def index
+    @default_repo = 'https://github.com/rails/rails/'
+  end
+  def awords() end
+  def pdf() end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'awords#index'
+
+  get 'awords' => 'awords#awords'
+  get 'aword' => 'awords#pdf'
 end
