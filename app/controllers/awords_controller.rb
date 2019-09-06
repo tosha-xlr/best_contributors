45314d4fb32516093d7061b8b5f4182ad8e69b67
Anton Kuzelenkov <kuzelenkov@gmail.com>
2019-09-06 10:56:21 +0300

    bug fix

--- app/controllers/awords_controller.rb
+++ app/controllers/awords_controller.rb
@@ -2,6 +2,9 @@ class AwordsController < ApplicationController
   def index
     @default_repo = 'https://github.com/rails/rails/'
   end
-  def awords() end
+  def awords
+    @repo_name =  params[:repo] ? params[:repo][:repo_name] : 'https://github.com/rails/rails/'
+    @repo_api_name = "https://api.github.com/repos" + @repo_name[18..-1] + "contributors?sort=contributions&sort=order&per_page=3"
+  end
   def pdf() end
 end
